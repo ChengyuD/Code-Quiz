c2eddabd43a874b2ca35280c290fc9796d703a76
Cheryth D <80147201+ChengyuD@users.noreply.github.com>
2021-04-10 21:30:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -108,15 +108,15 @@ function checkAnswer(event) {
     var buttonEl = event.target;
     if (buttonEl) {
         var buttonChosen = parseInt(buttonEl.getAttribute("selectedIndex"));
-        var answerChoice = questions[currentQuestionIndex].answer;
+        var answerChoice = quiz[quizQuestion].answer;
         
         if (buttonChosen === answerChoice) {
             feedbackEl.textContent = "CORRECT!";
-            score += 5;
+            score += 10;
         
         } else if (buttonChosen != answerChoice) {
             feedbackEl.textContent = "WRONG!";
-            timer -= 10;
+            timer -= 5;
             
             if (timer <= 0) {
                 timesUp();
@@ -131,114 +131,3 @@ function checkAnswer(event) {
         }
     }
 }
-var feedbackEl = document.getElementById("question-feedback");
-
-function feedBackTimeout() {
-    feedbackEl.setAttribute("class", "hidden");
-    getNextQuestion();
-}
-// check timer again before incrementing questions array index
-function getNextQuestion() {
-    if (timer <= 0) {
-        roundOver();
-        return;
-    } else {
-        ++currentQuestionIndex;
-    }
-    // if index indicates no more questions quiz over, otherwise clear current question and build next question
-    if (currentQuestionIndex >= questions.length) {
-        roundOver();
-    } else {
-        clearAnswers();
-        buildQuiz();
-    }
-}
-// if quiz round over set timer to 0, display text round over message 
-function roundOver() {
-    timer = 0;
-    countDownEl.textContent = "Round Over!";
-    // clear starting timer, last question displayed, prepare to end quiz
-    clearInterval(countDownTimer);
-    clearAnswers();
-    endQuiz();
-}
-// clear answer buttons by looping through choices, removing answer button at first index
-// so as array shrinks, we are not trying to access a non existent index
-function clearAnswers() {
-    var count = quizAnswersEl.childElementCount;
-    for (var i = 0; i < count; i++) {
-        quizAnswersEl.removeChild(quizAnswersEl.childNodes[0]);
-    }
-}
-var quizEndEl = document.getElementById("quiz-end");
-var submitButton = document.getElementById("submit-button");
-// hide question screen, unhide recording score screen and display final score
-function endQuiz() {
-    quizQuestionEl.classList.add("hidden");
-    quizEndEl.classList.remove("hidden");
-    var finalScoreEl = document.getElementById("final-score");
-    finalScoreEl.textContent = score;
-    // after user inputs initials and clicks submit button event to save score triggered
-    submitButton.addEventListener("click", getInitials);
-}
-// user initials input is validated, if  this is first time quiz is played, an array is created to hold score data
-// initials and score are saved to local strorage if user current score is higher than last recorded high score
-const scoreName = "endscore";
-var initialsEl = document.getElementById("initials");
-
-function getInitials() {
-    if (!initialsEl || initialsEl.value === "") {
-        alert("You must enter your initials");
-        return;
-    } else {
-        var lastHighScore = localStorage.getItem(scoreName);
-        var lastHighScoreArray = JSON.parse(lastHighScore);
-        if (!lastHighScoreArray || score > lastHighScoreArray[0].newScore) {
-            var scoreData = {
-                name: initialsEl.value,
-                newScore: score
-            };
-            if (!lastHighScoreArray) lastHighScoreArray = [];
-            lastHighScoreArray.push(scoreData);
-            lastHighScoreArray.sort(function (a, b) {
-                return -(a.newScore - b.newScore)
-            });
-            localStorage.setItem(scoreName, JSON.stringify(lastHighScoreArray));
-        }
-    }
-    showResults();
-}
-// initials screen is hidden and results from local storage are displayed, 
-var highResultEl = document.getElementById("show-result");
-
-function showResults() {
-    quizEndEl.classList.add("hidden");
-    highResultEl.classList.remove("hidden");
-    var showHighResultEl = document.querySelector("#show-high-result");
-    var lastHighScore = localStorage.getItem(scoreName);
-    lastHighScoreArray = JSON.parse(lastHighScore);
-    if (lastHighScoreArray) {
-        showHighResultEl.value = "1. " + lastHighScoreArray[0].name + ":" + lastHighScoreArray[0].newScore;
-    }
-};
-// user now has choice to clear all scores in storage
-function clearLocalStorage() {
-    document.querySelector("#show-high-result").value = "";
-    document.querySelector("#view-high-score").textContent = 0;
-    localStorage.clear(lastHighScoreArray);
-}
-// and/or start quiz over, score and question index are reset
-function startGameOver() {
-    highResultEl.classList.add("hidden");
-    startPageEl.classList.remove("hidden")
-    initialsEl.value = "";
-    currentQuestionIndex = 0;
-    score = 0;
-}
-// event handlers for start, go back(start game over) and clear all scores from storage
-var startBtn = document.querySelector("#start-button");
-var goBackBtn = document.querySelector("#go-back");
-var clearResultBtn = document.querySelector("#clear-result");
-startBtn.addEventListener("click", countDown);
-goBackBtn.addEventListener("click", startGameOver);
-clearResultBtn.addEventListener("click", clearLocalStorage);
